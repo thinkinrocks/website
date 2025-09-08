1abd29b1e455854a1b2f1e559e65866981503253
Yerzhan Zhamashev <yerzhan@novel.systems>
2025-09-08 19:03:33 +0300

    Add welcome buttons component and add button tracking
    - Introduced a new `WelcomeButtons` component to encapsulate button links for events and Discord.
    - Updated `ButtonLink` and `DiscordButton` components to accept an optional `onClick` prop for tracking button interactions.
    - Refactored the `Welcome` component to utilize the new `WelcomeButtons` for a cleaner layout and improved user experience.

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -3,15 +3,19 @@ import { FaDiscord } from "react-icons/fa";
 export function ButtonLink({
   href = "#",
   children,
+  onClick,
 }: {
   href: string;
   children: React.ReactNode;
+  onClick?: () => void;
 }) {
   return (
     <div className="bg-slate-600 h-fit translate-0.5">
       <a
         href={href}
         className="bg-slate-50 text-slate-600 inline-flex px-4 py-2 -translate-0.5 items-center gap-2 whitespace-nowrap"
+        target="_blank"
+        onClick={onClick}
       >
         {children}
       </a>
@@ -19,12 +23,20 @@ export function ButtonLink({
   );
 }
 
-export function DiscordButton({ href = "#" }: { href: string }) {
+export function DiscordButton({
+  href = "#",
+  onClick,
+}: {
+  href: string;
+  onClick?: () => void;
+}) {
   return (
     <div className="bg-green-600 h-fit translate-0.5">
       <a
         href={href}
         className="bg-green-50 text-green-600 inline-flex px-4 py-2 -translate-0.5 items-center gap-2"
+        target="_blank"
+        onClick={onClick}
       >
         <FaDiscord className="w-4 h-4 -mb-1" />
         Discord
@@ -36,15 +48,19 @@ export function DiscordButton({ href = "#" }: { href: string }) {
 export function EventButton({
   href = "#",
   children,
+  onClick,
 }: {
   href: string;
   children: React.ReactNode;
+  onClick?: () => void;
 }) {
   return (
     <div className="bg-indigo-600 h-fit translate-0.5">
       <a
         href={href}
         className="bg-indigo-50 text-indigo-600 inline-flex px-4 py-2 -translate-0.5 items-center gap-2 whitespace-nowrap"
+        target="_blank"
+        onClick={onClick}
       >
         {children}
       </a>
