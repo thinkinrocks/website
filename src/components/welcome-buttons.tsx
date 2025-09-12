6555c4c99d9240f10d21b69731c4fc405c139736
Milana <milka3341@gmail.com>
2025-09-12 20:23:02 +0300

    Add new font and images, update layout and components
    - Added 'Space Grotesk' font to package.json and updated global styles.
    - Introduced 'Departure Mono' font with corresponding CSS.
    - Added new images for team members and equipment.
    - Created new pages for 'Equipment', 'Team', and 'Questions' with relevant content.
    - Implemented a fixed Navbar component for site navigation.
    - Updated Welcome component and buttons for improved user experience.

--- src/components/welcome-buttons.tsx
+++ src/components/welcome-buttons.tsx
@@ -1,4 +1,4 @@
-import { FaCalendarAlt, FaDiscord, FaUserPlus } from "react-icons/fa";
+import { FaCalendarAlt, FaDiscord, FaUserPlus, FaTelegram } from "react-icons/fa";
 import { ButtonLink, EventButton } from "./buttons.tsx";
 import { track } from "@vercel/analytics";
 
@@ -10,22 +10,30 @@ export default function WelcomeButtons() {
         onClick={() => track("meetup_button_click")}
       >
         <FaCalendarAlt className="w-4 h-4 -mb-0.5" />
-        Join meetup 16.09
+        <span className="font-mono">Join meetup 16.09</span>
       </EventButton>
       <ButtonLink
         href="https://discord.gg/gtncJfm85n"
         onClick={() => track("discord_button_click")}
       >
         <FaDiscord className="w-4 h-4 -mb-0.5" />
-        Discord
+        <span className="font-mono">Discord</span>
+      </ButtonLink>
+      <ButtonLink
+        href="https://t.me/thinkinrocks"
+        onClick={() => track("telegram_button_click")}
+      >
+        <FaTelegram className="w-4 h-4 -mb-0.5" />
+        <span className="font-mono">Telegram</span>
       </ButtonLink>
       <ButtonLink
         href="https://tally.so/r/mBrVg7"
         onClick={() => track("signup_button_click")}
       >
         <FaUserPlus className="w-4 h-4 -mb-0.5" />
-        Show interest
+        <span className="font-mono">Show interest</span>
       </ButtonLink>
+     
     </div>
   );
 }
