bf9e31f9d125c09c62961d01619461c5aa9b6b70
Milana <milka3341@gmail.com>
2025-09-20 18:25:36 +0300

    Update WelcomeButtons component with new button links and text
    - Removed the meetup button and updated the existing Discord button.
    - Added a new Telegram chat button with updated text for clarity.
    - Enhanced user interaction tracking for the new buttons.

--- src/components/welcome-buttons.tsx
+++ src/components/welcome-buttons.tsx
@@ -6,25 +6,25 @@ export default function WelcomeButtons() {
   return (
     <div className="flex flex-wrap justify-center gap-2">
       <EventButton
-        href="https://luma.com/sf4dp6u2"
-        onClick={() => track("meetup_button_click")}
-      >
-        <FaCalendarAlt className="w-4 h-4 -mb-0.5" />
-        <span className="font-mono">Join meetup 16.09</span>
-      </EventButton>
-      <ButtonLink
         href="https://discord.gg/gtncJfm85n"
         onClick={() => track("discord_button_click")}
       >
         <FaDiscord className="w-4 h-4 -mb-0.5" />
         <span className="font-mono">Discord</span>
-      </ButtonLink>
+      </EventButton>
       <ButtonLink
         href="https://t.me/thinkinrocks"
         onClick={() => track("telegram_button_click")}
       >
         <FaTelegram className="w-4 h-4 -mb-0.5" />
-        <span className="font-mono">Telegram</span>
+        <span className="font-mono">Telegram channel</span>
+      </ButtonLink>
+      <ButtonLink
+        href="https://t.me/+rxHNt6GA4-o1NWUy"
+        onClick={() => track("telegram_button_click")}
+      >
+        <FaTelegram className="w-4 h-4 -mb-0.5" />
+        <span className="font-mono">Telegram chat</span>
       </ButtonLink>
       <ButtonLink
         href="https://tally.so/r/mBrVg7"
