7a2ca9f546d6a026dc7f2438d28bedf08b15fb4c
Artur Roos <artur.roos@ktnlvr.dev>
2026-03-09 14:53:30 +0200

    Extract logs into proper .mdx files

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,7 +8,20 @@ const initiatives = defineCollection({
   }),
 });
 
+const log = defineCollection({
+  type: "content",
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    author: z.string(),
+    timestamp: z.string(),
+    tags: z.array(z.string()),
+    type: z.string(),
+  }),
+});
+
 export const collections = {
   initiatives,
+  log,
 };
 
